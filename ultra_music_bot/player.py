4954d29635c57cc35787e23af44a7a023ba0e140
Nishkarsh Kumar <nishkarshk212@gmail.com>
2026-03-06 09:34:12 +0530

    fallback: add DummyCall to keep bot responding without tgcalls

--- ultra_music_bot/player.py
+++ ultra_music_bot/player.py
@@ -3,21 +3,25 @@ import config
 from pyrogram import Client
 from userbot import user
 _PTG_V3 = False
+_ENGINE_AVAILABLE = False
 try:
     from tgcalls import GroupCallFactory
     from tgcalls.types import StreamType
     from tgcalls.types.input_stream import AudioPiped
     _PTG_V3 = True
+    _ENGINE_AVAILABLE = True
 except Exception:
     try:
         from pytgcalls import GroupCallFactory
         from pytgcalls.types import StreamType
         from pytgcalls.types.input_stream import AudioPiped
         _PTG_V3 = True
+        _ENGINE_AVAILABLE = True
     except Exception:
         from pytgcalls import PyTgCalls
         from pytgcalls.types import MediaStream, AudioQuality
         _PTG_V3 = False
+        _ENGINE_AVAILABLE = True
 
 app = Client(
     config.SESSION,
@@ -62,16 +66,51 @@ if _PTG_V3:
             return list(self.calls.keys())
     call = CallAdapter(user)
 else:
-    call = PyTgCalls(user)
+    class CallAdapterV2:
+        def __init__(self, client):
+            self.impl = PyTgCalls(client)
+        def start(self):
+            return self.impl.start()
+        def stop(self):
+            return self.impl.stop()
+        async def play(self, chat_id, stream_obj):
+            await self.impl.play(chat_id, stream_obj)
+        async def change_stream(self, chat_id, stream_obj):
+            await self.impl.change_stream(chat_id, stream_obj)
+        async def pause(self, chat_id):
+            await self.impl.pause(chat_id)
+        async def resume(self, chat_id):
+            await self.impl.resume(chat_id)
+        async def leave_call(self, chat_id):
+            await self.impl.leave_call(chat_id)
+        async def get_active_chats(self):
+            return await self.impl.get_active_chats()
+    call = CallAdapterV2(user)
+
+if not _ENGINE_AVAILABLE:
+    class DummyCall:
+        def start(self): return None
+        def stop(self): return None
+        async def play(self, chat_id, stream): return None
+        async def change_stream(self, chat_id, stream): return None
+        async def pause(self, chat_id): return None
+        async def resume(self, chat_id): return None
+        async def leave_call(self, chat_id): return None
+        async def get_active_chats(self): return []
+    call = DummyCall()
 
 async def start_stream(chat_id, stream):
     if _PTG_V3:
         await call.play(chat_id, stream)
-    else:
+    elif _ENGINE_AVAILABLE:
         await call.play(chat_id, MediaStream(stream, audio_parameters=AudioQuality.HIGH))
+    else:
+        return None
 
 async def change_stream(chat_id, stream):
     if _PTG_V3:
         await call.change_stream(chat_id, stream)
-    else:
+    elif _ENGINE_AVAILABLE:
         await call.change_stream(chat_id, MediaStream(stream, audio_parameters=AudioQuality.HIGH))
+    else:
+        return None
