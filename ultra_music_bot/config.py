569bcda5c23d61001b3090298ff9a7734462d6c9
Nishkarsh Kumar <nishkarshk212@gmail.com>
2026-03-06 10:39:28 +0530

    Default USERBOT_ENABLED=0 to prevent unintended user client start

--- ultra_music_bot/config.py
+++ ultra_music_bot/config.py
@@ -16,4 +16,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 SESSION_STRING = os.getenv("SESSION_STRING", "")
 SESSION = os.getenv("SESSION", "ultra_music_bot_session")
 OWNER_ID = int(os.getenv("OWNER_ID", "0"))
-USERBOT_ENABLED = os.getenv("USERBOT_ENABLED", "1") == "1"
+USERBOT_ENABLED = os.getenv("USERBOT_ENABLED", "0") == "1"
