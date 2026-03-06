e45f57c48d9f7e049d4ad7aaf6a4c12ca9d5b16f
Nishkarsh Kumar <nishkarshk212@gmail.com>
2026-03-06 09:30:31 +0530

    compat: add GroupCallFactory adapter for pytgcalls v3

--- ultra_music_bot/player.py
+++ ultra_music_bot/player.py
@@ -2,19 +2,16 @@
 import config
 from pyrogram import Client
 from userbot import user
+_PTG_V3 = False
 try:
+    from pytgcalls import GroupCallFactory
+    from pytgcalls.types import StreamType
+    from pytgcalls.types.input_stream import AudioPiped
+    _PTG_V3 = True
+except Exception:
     from pytgcalls import PyTgCalls
     from pytgcalls.types import MediaStream, AudioQuality
     _PTG_V3 = False
-except Exception:
-    try:
-        from pytgcalls import GroupCall as PyTgCalls
-        from pytgcalls.types import MediaStream, AudioQuality
-        _PTG_V3 = True
-    except Exception:
-        from pytgcalls import PyTgCalls
-        from pytgcalls.types import MediaStream, AudioQuality
-        _PTG_V3 = False
 
 app = Client(
     config.SESSION,
@@ -24,16 +21,51 @@ app = Client(
 )
 
 # Use user client for PyTgCalls to allow group calls
-call = PyTgCalls(user)
+if _PTG_V3:
+    class CallAdapter:
+        def __init__(self, client):
+            self.client = client
+            self.factory = GroupCallFactory(client)
+            self.calls = {}
+        def start(self):
+            return None
+        def stop(self):
+            return None
+        async def play(self, chat_id, stream_url):
+            if chat_id not in self.calls:
+                self.calls[chat_id] = self.factory.get_group_call()
+                await self.calls[chat_id].join(chat_id, AudioPiped(stream_url), stream_type=StreamType().local_stream)
+            else:
+                await self.calls[chat_id].change_stream(AudioPiped(stream_url))
+        async def change_stream(self, chat_id, stream_url):
+            if chat_id in self.calls:
+                await self.calls[chat_id].change_stream(AudioPiped(stream_url))
+        async def pause(self, chat_id):
+            if chat_id in self.calls:
+                await self.calls[chat_id].pause_stream()
+        async def resume(self, chat_id):
+            if chat_id in self.calls:
+                await self.calls[chat_id].resume_stream()
+        async def leave_call(self, chat_id):
+            if chat_id in self.calls:
+                try:
+                    await self.calls[chat_id].leave()
+                finally:
+                    self.calls.pop(chat_id, None)
+        async def get_active_chats(self):
+            return list(self.calls.keys())
+    call = CallAdapter(user)
+else:
+    call = PyTgCalls(user)
 
 async def start_stream(chat_id, stream):
-    await call.play(
-        chat_id,
-        MediaStream(stream, audio_parameters=AudioQuality.HIGH),
-    )
+    if _PTG_V3:
+        await call.play(chat_id, stream)
+    else:
+        await call.play(chat_id, MediaStream(stream, audio_parameters=AudioQuality.HIGH))
 
 async def change_stream(chat_id, stream):
-    await call.change_stream(
-        chat_id,
-        MediaStream(stream, audio_parameters=AudioQuality.HIGH),
-    )
+    if _PTG_V3:
+        await call.change_stream(chat_id, stream)
+    else:
+        await call.change_stream(chat_id, MediaStream(stream, audio_parameters=AudioQuality.HIGH))
